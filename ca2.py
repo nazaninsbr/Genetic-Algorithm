fe6076ac1270923f3142a96f4bdee149294d98d5
tinayz <yazdizadeh.tina@gmail.com>
2018-04-09 11:26:02 +0430

    calHappiness and readHappiness and conflict done

--- ca2.py
+++ ca2.py
@@ -1,19 +1,44 @@
 class Course:
-	def __init__(self):
+	def __init__(self , value):
 		self.course_id = -1
 		self.time = -1
 		self.day = -1
+		# for course number i ci is value of happiness
+		self.happiness_value = value
+		self.offer = 1
+		self.conflictCourses = []
+		self.has_conflict = 1
+
+	def conflict(self , Course course);
+		conflict_course = list()
+		if self.time == course.time and self.day == course.day:
+			self.conflictCourses.append(course.course_id)
+			course.has_conflict = 0
+			self.has_conflict = 0
+
 
 class Students:
 	def __init__(self):
 		# ci 
 		self.hapiness = []
 		# k i j 
-		self.sadness = [][]
-
+		self.sadness = []
+		self.ownCourse = []
+	
 	def readHappiness():
-		line = input()
-		#separate by space 
+		valueofHappiness =  map(int, input().split())
+		hapiness.append(valueofHappiness)
+
+	def calHappiness(self , Course courses =[]):
+		totalValue = 0
+		for i in range (0 , len(courses)):
+			if courses[i].offer != 1 :
+				totalValue += courses[i].happiness_value
+		return totalValue
+	
+	#def calSadness():
+
+
 
 class Professor:
 	def __init__(self):
@@ -24,6 +49,7 @@ class Professor:
 
 class University:
 	def __init__(self):
+		#list of course and prof
 		self.profs = []
 		self.courses = []
 		# c
@@ -34,14 +60,12 @@ class University:
 		self.TIME_SLOT = 0
 		# d
 		self.DAY_SLOT = 0
-		self.students = Students()
+		self.students = []
 		
 	def readInput():	
 		self.DAY_SLOT, self.TIME_SLOT = input()
 		self.NUMBER_OF_COURSES = input()
 		self.students.readHappiness()
-
-
 
 
 
